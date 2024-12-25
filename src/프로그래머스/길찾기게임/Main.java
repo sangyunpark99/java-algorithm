5f1d72ad9247582f7fd9ab528a8a4fefd9d40c96
psangyun5108@gmail.com <psangyun5108@gmail.com>
2024-12-25 23:19:05 +0900

    프로그래머스lv3-경주로건설

---
+++ src/프로그래머스/길찾기게임/Main.java
@@ -0,0 +1,17 @@
+package 프로그래머스.길찾기게임;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Solution().solution(new int[][]{
+                {5, 3},
+                {11, 5},
+                {13, 3},
+                {3, 5},
+                {6, 1},
+                {1, 3},
+                {8, 6},
+                {7, 2},
+                {2, 2}
+        }));
+    }
+}
---
+++ src/프로그래머스/길찾기게임/Solution.java
@@ -0,0 +1,9 @@
+package 프로그래머스.길찾기게임;
+
+class Solution {
+    public int[][] solution(int[][] nodeinfo) {
+        int[][] answer = {};
+
+        return answer;
+    }
+}
