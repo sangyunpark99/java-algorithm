a15dca71fdd092f7bcf68e40a2f0541c4936d700
박상윤 <96441638+sangyunpark99@users.noreply.github.com>
2024-12-28 09:07:16 +0900

    [level 2] Title: 스킬트리, Time: 0.31 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String skill, String[] skill_trees) {
+        int answer = 0;
+        
+        char[] link = skill.toCharArray(); // 인덱스 = 순서
+        
+        for(String value: skill_trees) {
+            
+            List<String> order = new ArrayList<>();
+            
+            for(char c: value.toCharArray()) {
+                if(skill.contains(String.valueOf(c)) && !order.contains(String.valueOf(c))) {
+                    order.add(String.valueOf(c));
+                }
+            }
+            
+            int checkIdx = 0;
+            boolean check = true;
+            for(int i = 0; i < order.size(); i++) {
+                if(order.get(i).equals(String.valueOf(link[checkIdx]))) {
+                    checkIdx++;
+                    continue;
+                }
+                
+                check = false;
+                break;
+            }
+            
+            if(check) {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
