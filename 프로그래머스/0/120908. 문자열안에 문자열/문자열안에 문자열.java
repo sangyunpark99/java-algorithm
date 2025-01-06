a903e8910abd326fd34e400d9b86fa5d78ec505d
박상윤 <96441638+sangyunpark99@users.noreply.github.com>
2025-01-06 09:24:25 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.04 ms, Memory: 85.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String str1, String str2) {
+        int answer = 0;
+        
+        if(str1.contains(str2)) {
+            return 1;
+        }
+        
+        return 2;
+    }
+}
