dd271177f3e614fbcaffefb9985be63022e7356a
psangyun5108@gmail.com <psangyun5108@gmail.com>
2025-01-21 09:12:27 +0900

    프로그래머스lv3-외벽점검

---
+++ src/프로그래머스/lv3외벽점검/Solution.java
@@ -0,0 +1,18 @@
+package 프로그래머스.lv3외벽점검;
+
+public class Solution {
+    int[][][] dp; // 시작지점, 도착지점, 허물어진 벽 포함 갯수
+
+    public int solution(int n, int[] weak, int[] dist) {
+        int answer = 0;
+
+        for (int distValue : dist) { // O(n) = 15
+            for (int i = 0; i < n; i++) { // O(n) = 200
+                // O(n) = 3000
+
+            }
+        }
+
+        return answer;
+    }
+}
