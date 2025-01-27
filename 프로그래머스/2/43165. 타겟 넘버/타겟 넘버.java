1e465cf4914b371611c1c71f8394361d44516972
박상윤 <96441638+sangyunpark99@users.noreply.github.com>
2025-01-27 23:39:48 +0900

    [level 2] Title: 타겟 넘버, Time: 10.70 ms, Memory: 90.8 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -2,33 +2,26 @@ class Solution {
     
     static int[] Numbers;
     static int Target;
-    //static int answer = 0;
     
     public int solution(int[] numbers, int target) {
         
         Numbers = numbers;
         Target = target;
-        
-        // 더하거나 빼거나
-        
+
         return dfs(0, 0);
     }
     
     private int dfs(int curValue, int curIdx) {
         
-        int answer = 0;
-        
         if(curIdx == Numbers.length) {
             
             if(curValue == Target) {
-                answer++;        
+                return 1;        
             }
             
-            return answer;
+            return 0;
         }
         
-        answer = dfs(curValue + Numbers[curIdx], curIdx+1) + dfs(curValue - Numbers[curIdx], curIdx+1);
-        
-        return answer;
+        return dfs(curValue + Numbers[curIdx], curIdx+1) + dfs(curValue - Numbers[curIdx], curIdx+1);
     }
 }
