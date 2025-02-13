e8051b36b29341e46f0401cdeacd424b0a5a27a8
박상윤 <96441638+sangyunpark99@users.noreply.github.com>
2025-02-13 14:05:14 +0900

    [level 2] Title: 조이스틱, Time: 0.07 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -3,19 +3,16 @@ import java.util.*;
 class Solution {
     public int solution(String name) {
         int answer = 0;
-        int length = name.length();
         
-        // 1. 알파벳 변경 최소 횟수 계산
-        for (char ch : name.toCharArray()) { // A부터 or Z부터
-            answer += Math.min(ch - 'A', 'Z' - ch + 1);
+        for(char c : name.toCharArray()) {
+            answer += Math.min(c - 'A', 'Z' - c + 1);
         }
         
-        // 2. 커서 이동 최소 횟수 계산
-        int move = name.length() - 1;
-
+        int length = name.length();
+        int move = length - 1;
+        
         for(int i = 0; i < length; i++) {
             int next = i + 1;
-            
             while(next < length && name.charAt(next) == 'A') {
                 next++;
             }
@@ -24,6 +21,7 @@ class Solution {
         }
         
         answer += move;
+        
         return answer;
     }
 }
