5e176700179712f3f30fe1d2d006e65d6085dca7
sangyunpark99 <psangyun5108@gmail.com>
2025-04-02 18:13:31 +0900

    백준 빗물

---
+++ src/백준/빗물/Main.java
@@ -0,0 +1,48 @@
+package 백준.빗물;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int H = Integer.parseInt(st.nextToken());
+        int W = Integer.parseInt(st.nextToken());
+
+        int[] height = new int[W];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < W; i++) {
+            height[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int left = 0;
+        int right = W - 1;
+        int leftMax = 0;
+        int rightMax = 0;
+        int totalWater = 0;
+
+        while (left < right) {
+            if (height[left] < height[right]) { // 왼쪽 벽보다 오른쪽 벽이 더 높은 경우
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    totalWater += leftMax - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    totalWater += rightMax - height[right];
+                }
+                right--;
+            }
+        }
+
+        System.out.println(totalWater);
+    }
+}
