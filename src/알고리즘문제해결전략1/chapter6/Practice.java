47024e8e7611d352dc805bbbf6d516567162ccc6
sangyunpark99 <psangyun5108@gmail.com>
2025-05-23 00:09:49 +0900

    프로그래머스 퍼즐게임챌린지

---
+++ src/알고리즘문제해결전략1/chapter6/Practice.java
@@ -0,0 +1,10 @@
+package 알고리즘문제해결전략1.chapter6;
+
+public class Practice {
+    public static void main(String[] args) {
+        Integer a = 10;
+        Integer b = 20;
+
+        System.out.println(a.equals(b));
+    }
+}
---
+++ src/프로그래머스/퍼즐게임챌린지/Solution.java
@@ -0,0 +1,4 @@
+package 프로그래머스.퍼즐게임챌린지;
+
+public class Solution {
+}
