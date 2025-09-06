48b93f886247db6f6150fc7b0492e270fba463b5
박상윤 <96441638+sangyunpark99@users.noreply.github.com>
2025-09-06 23:30:21 +0900

    [level 3] Title: 징검다리 건너기, Time: 23.26 ms, Memory: 64.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+class Solution {
+    
+    private int[] stonesCopy;
+    private int K;
+    private int answer;
+    
+    public int solution(int[] stones, int k) {
+        
+        stonesCopy = stones;
+        K = k;
+        binarySearch(); // O(nlogn)
+        
+        return answer;
+    }
+    
+    private void binarySearch() { // O(logn)
+        
+        int left = 1;
+        int right = 200_000_000;
+        
+        while(left <= right) {
+            int mid = (left + right) / 2;
+            
+            if(check(mid)) {
+                left = mid + 1;
+                answer = mid;
+            }else {
+                right = mid - 1;
+            }
+        }
+    }
+    
+    private boolean check(int value) { // 현재 인원 통과 가능 한가?
+        
+        int cnt = 0;
+        // 연속으로 밟을 수 없는 돌이 K개 이상인 경우
+        for(int stone : stonesCopy) {
+            if(stone < value) {
+                cnt++;
+                if(cnt >= K) return false;
+            }else {
+                cnt = 0;
+            }
+        }
+        
+        return true;
+    }
+}
