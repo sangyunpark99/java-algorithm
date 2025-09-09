5545d2ac9a247852bdb5bbd2827d0910c8664893
박상윤 <96441638+sangyunpark99@users.noreply.github.com>
2025-09-09 17:29:50 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 4.53 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
@@ -2,13 +2,15 @@ import java.util.*;
 
 class Solution {
     
-    private int[][][] cost;
-    private int INF = 1_000_000_000;
-    private int[] dy = new int[] {-1, 0, 1, 0};
-    private int[] dx = new int[] {0, 1, 0, -1};
-    private int n;
+    // 가중치가 다른 최소 비용 
     
-    public static class Node {
+    private int[][][] cost;
+    private int[] dy = new int[] {-1,0,1,0};
+    private int[] dx = new int[] {0,1,0,-1};
+    private int n;
+    private int INF = 1_000_000_000;
+    
+    private static class Node {
         int y;
         int x;
         int cost;
@@ -23,45 +25,48 @@ class Solution {
     }
     
     public int solution(int[][] board) {
-        
         int answer = INF;
-        n = board.length;
-        cost = new int[board.length][board.length][4];
         
-        for(int i = 0; i < board.length; i++) {
-            for(int j = 0; j < board.length; j++) {
+        n = board.length;
+        
+        cost = new int[n][n][4];
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
                 Arrays.fill(cost[i][j], INF);
             }
         }
         
-        // y, x, cost, dir
+        // 0(상), 1(오), 2(아), 3(왼)
+        
         PriorityQueue<Node> pq = new PriorityQueue<>((a,b) -> Integer.compare(a.cost, b.cost));
         
-        if(board[0][1] == 0) { // 오른쪽으로 출발하는 경우
+        if(board[0][1] == 0) {
             cost[0][1][1] = 100;
-            pq.offer(new Node(0, 1, 100, 1));
+            pq.offer(new Node(0,1,100,1));
         }
         
-        if(board[1][0] == 0) { // 아래로 출발하는 경우
+        if(board[1][0] == 0) {
             cost[1][0][2] = 100;
-            pq.offer(new Node(1, 0, 100, 2));
+            pq.offer(new Node(1,0,100,2));
         }
         
         while(!pq.isEmpty()) {
+           
             Node cur = pq.poll();
-            if(cur.cost > cost[cur.y][cur.x][cur.dir]) continue;
+            if(cost[cur.y][cur.x][cur.dir] < cur.cost) continue; // 느긋한 삭제
             
-            for(int i = 0; i < 4; i++) {
+            for(int i = 0; i < 4; i++) { // 방향 
                 int ny = cur.y + dy[i];
                 int nx = cur.x + dx[i];
                 
-                if(ny <  0 || ny >= n || nx < 0 || nx >= n) continue;
-                if(board[ny][nx] == 1) continue; // 벽인 경우 패스
+                if(ny < 0 || ny >= n || nx < 0 || nx >= n) continue;
+                if(board[ny][nx] == 1) continue;
                 
-                int addCost = i == cur.dir ? 100 : 600;
+                int addCost = cur.dir == i ? 100 : 600;
                 int nCost = cur.cost + addCost;
                 
-                if(nCost < cost[ny][nx][i]) {
+                if(cost[ny][nx][i] > nCost) { // 더 작은 비용이 드는 경로인 경우만
                     cost[ny][nx][i] = nCost;
                     pq.offer(new Node(ny, nx, nCost, i));
                 }
